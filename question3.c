703456bd5f07fdd74ad126eb58be86788a4e6f37
Wafia <wafiashafiq@gmail.com>
2026-04-17 19:14:25 +0530

    basic calculator

---
+++ question3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int y = 5;
+
+    int sum = x + y;
+    printf("%d\n", sum);
+
+    int difference = x - y;
+    printf("%d\n", difference);
+
+    int product = x * y;
+    printf("%d\n", product);
+
+    int quotient = x / y;
+    printf("%d\n", quotient);
+
+    return 0;
+}
