0be2a127b1b679e054c06474d6f179745779bb98
Wafia <wafiashafiq@gmail.com>
2026-06-22 15:10:16 +0530

    square series

---
+++ question20.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    int sum = 0;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++)
+    {
+        sum = sum + (i * i);
+    }
+    printf("Sum of square series = %d", sum);
+    return 0;
+}
